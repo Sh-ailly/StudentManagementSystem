bc78bdcf5aa6316a8902b04863d11f1988d83573
Anurag Jain <anuragjain353@gmail.com>
2024-04-25 13:27:31 +0530

    This is the new commit

--- app/src/main/java/com/example/studentmanagementsystem/ui/main/Contact_Admin_Page.java
+++ app/src/main/java/com/example/studentmanagementsystem/ui/main/Contact_Admin_Page.java
@@ -3,7 +3,6 @@ package com.example.studentmanagementsystem.ui.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.net.Uri;
 import android.os.Bundle;
 
 import com.example.studentmanagementsystem.MainActivity;
@@ -14,12 +13,8 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.*;
 
-import java.util.Properties;
-
 public class Contact_Admin_Page extends AppCompatActivity {
-    ImageView back;
-    Button button;
-
+ImageView back;
     @SuppressLint({"MissingInflatedId", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +23,13 @@ public class Contact_Admin_Page extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_contact_admin_page);
-        back = (ImageView) findViewById(R.id.backbuttonimage);
+        back=(ImageView)findViewById(R.id.backbuttonimage);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Contact_Admin_Page.this, MainActivity.class);
+                Intent intent=new Intent(Contact_Admin_Page.this, MainActivity.class);
                 startActivity(intent);
             }
         });
-
     }
 }
