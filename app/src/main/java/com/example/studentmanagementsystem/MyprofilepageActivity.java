91a2c8156c0407b68d87ce34bebc567a739dc899
Shailly <shaillypalariya21@gmail.com>
2024-04-25 18:38:37 +0530

    my profile page

--- app/src/main/java/com/example/studentmanagementsystem/MyprofilepageActivity.java
+++ app/src/main/java/com/example/studentmanagementsystem/MyprofilepageActivity.java
@@ -1,6 +1,12 @@
 package com.example.studentmanagementsystem;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MyprofilepageActivity extends AppCompatActivity {
+import com.example.studentmanagementsystem.ui.main.Contact_Admin_Page;
 
+public class MyprofilepageActivity extends AppCompatActivity {
+ImageView back;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this is for hiding the action bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.myprofilepage);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        back=(ImageView)findViewById(R.id.profilebackbtn);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MyprofilepageActivity.this, Dashboard.class);
+                startActivity(intent);
+            }
         });
     }
 }
