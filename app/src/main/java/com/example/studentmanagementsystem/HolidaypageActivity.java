21adba7aaa2af66b24d55e17236c1bcda3456863
anurag1524 <anuragjain353@gmail.com>
2024-04-27 11:42:25 +0530

    Made the changes, code is working fine now

--- app/src/main/java/com/example/studentmanagementsystem/HolidaypageActivity.java
+++ app/src/main/java/com/example/studentmanagementsystem/HolidaypageActivity.java
@@ -1,6 +1,11 @@
 package com.example.studentmanagementsystem;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HolidaypageActivity extends AppCompatActivity {
-
+    ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this is for hiding the action bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.holidaypage);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        back=(ImageView)findViewById(R.id.imageView2);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(HolidaypageActivity.this, Dashboard.class);
+                startActivity(intent);
+            }
         });
     }
 }
